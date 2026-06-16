2e4aeeb28973a3850987e35349cb6db16d608ae5
amrw28940-byte <amrw28940@gmail.com>
2026-06-16 22:10:25 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // هذا سيسمح بمرور الـ Build حتى لو كان هناك أخطاء Types
     ignoreBuildErrors: true,
   },
-  // لا تضع eslint هنا، سنقوم بتعطيله بطريقة أخرى أدناه
 };
 
 export default nextConfig;
