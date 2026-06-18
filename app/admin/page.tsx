d9f3aa1c2fae85310a66bba4d64ec3efa44a5028
ANDALOS <amrw28940@gmail.com>
2026-06-18 20:27:13 +0300

    Modify article display page and fix connection errors

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = 'force-dynamic';
 import { useState, useEffect } from "react";
 import { createClient } from "@/lib/supabase"; // تم التعديل هنا
 import { Editor } from '@tinymce/tinymce-react';
